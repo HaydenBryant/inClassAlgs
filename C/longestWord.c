de0dc86d99cce29db7025355cd2d4604df7c8bfb
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-10-08 14:25:48 -0400

    longest word paper attempt. not complete

---
+++ C/longestWord.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+int main (void){
+    char string[100] = "The quick brown fox jumped over the lazy dog";
+    int count = 0;
+
+    for(int i = 0; i < strlen(string); i++){
+        
+    }
+}
+
