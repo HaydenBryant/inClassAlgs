ae171019feeb1e0705493c9278ce495c51e5e25f
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-10-10 11:55:10 -0400

    longestWord in c

--- C/longestWord.c
+++ C/longestWord.c
@@ -5,9 +5,18 @@
 int main (void){
     char string[100] = "The quick brown fox jumped over the lazy dog";
     int count = 0;
+    int longest = 0;
 
-    for(int i = 0; i < strlen(string); i++){
-        
+    for(int i = 0; string[i] != '\0'; i++){
+        if(string[i] != ' '){
+            count++;
+        }
+        if(count > longest){
+            longest = count;
+        }
+        if(string[i] == ' '){
+            count = 0;
+        }
     }
+    printf("longest word is %d letters long\n", longest);
 }
-
