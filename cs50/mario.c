c6cd63256776c4a3307349d440605ed4052f6e68
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-11-07 10:36:46 -0500

    setup

--- cs50/mario.c
+++ cs50/mario.c
@@ -1,29 +1,31 @@
 /* mario less */
 
-#include <cs50.h>
-#include <stdio.h>
+// #include <cs50.h>
+// #include <stdio.h>
 
-void print(int input){
-    for(int i = 0; i < input; i++){
-        for(int j = input; j > i; j--){
-            printf(" ");
-        }
-        for(int k = 0; k <= i; k++){
-            printf("#");
-        }
-        printf("\n");
-    }
-}
+// void print(int input){
+//     for(int i = 0; i < input; i++){
+//         for(int j = input; j > i; j--){
+//             printf(" ");
+//         }
+//         for(int k = 0; k <= i; k++){
+//             printf("#");
+//         }
+//         printf("\n");
+//     }
+// }
 
-int main(void){
-    int input = get_int("Height: ");
-    if(input < 1 || input > 8){
-        return main();
-    }
-    print(input);
-}
+// int main(void){
+//     int input = get_int("Height: ");
+//     if(input < 1 || input > 8){
+//         return main();
+//     }
+//     print(input);
+// }
 
 /* mario more */
+#include <cs50.h>
+#include <stdio.h>
 
 void hash(int i){
     for(int k = 0; k <= i; k++){
