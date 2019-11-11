ce1651299d5f3f62285c2f7df4e0f83d413b566d
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-11-11 13:18:19 -0500

    Starting caesar

---
+++ cs50/caesar.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    if(argc == 2)
+    {
+        const int KEY = atoi(argv[1]);
+
+    }
+    else
+    {
+        printf("Usage: ./caesar key");
+        return 1;
+    }
+}
