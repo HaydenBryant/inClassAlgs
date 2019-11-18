92e188b779ae653c2802bb084ec3ff15c80d8132
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-11-18 12:20:00 -0500

    vigenere

--- cs50/pset2/Vigenere.c
+++ cs50/pset2/Vigenere.c
@@ -7,24 +7,21 @@ int main(int argc, string argv[])
 {
     if(argc == 2)
     {
-        int 
         int length = strlen(argv[1]);
         for(int i = 0; i < length; i++)
         {
-            if (isdigit(argv[1][i]) == false)
+            if (isalpha(argv[1][i]) == 0)
             {
-                printf("Usage: ./caesar key");
+                printf("Usage: ./vigenere keyword");
                 return 1;
-            } else 
-            {
-                key = atoi(argv[1]);
             }
         }
-
+        string key = argv[1];
+        printf("%s", key);
     }
     else
     {
-        printf("Usage: ./caesar key");
+        printf("Usage: ./vigenere keyword");
         return 1;
     }
 }
