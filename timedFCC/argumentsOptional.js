738b16161be648bc569710b7e3bf1592d445f764
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-12-10 20:00:57 -0500

    begining class work binary tree

--- timedFCC/argumentsOptional.js
+++ timedFCC/argumentsOptional.js
@@ -7,7 +7,7 @@ function addTogether() {
     else if(args.length === 1  && Number.isInteger(args[0])){
         return function(arg2){
             if(Number.isInteger(arg2)){
-                console.log(args[0] + arg2);
+                return(args[0] + arg2);
             }
         }
     }
@@ -20,3 +20,24 @@ function addTogether() {
 //   addTogether(2)([3])
 //   addTogether(2, "3", 2);
 addTogether(2)(3);
+
+
+// function addTogether() {
+
+//     var args = Array.from(arguments);
+//     return args.forEach(arg => {
+//         // console.log(arg);
+//       if(Number.isInteger(arg) === false){
+//         //   console.log("undef")
+//           return undefined;
+//       } else if(args.length > 1){
+//         return(arg + args[1]);
+//       } else {
+//           return function(arg2){
+//             if(Number.isInteger(arguments[0])){
+//                 return(arg + arguments[0]);
+//             }
+//           }
+//       }
+//     });
+// }
