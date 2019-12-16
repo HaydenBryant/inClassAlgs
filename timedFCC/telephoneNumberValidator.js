25f4183f6afc0757d3a172e624e4779fc9fa818e
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-12-16 18:21:23 -0500

    telephoneValidator

--- timedFCC/telephoneNumberValidator.js
+++ timedFCC/telephoneNumberValidator.js
@@ -1,9 +1,29 @@
 function telephoneCheck(str) {
-    // Good luck!
-    var whiteSpace = /\s+|_+/g;
-    str = str.replace(whiteSpace, "");
-    return true;
+  // Good luck!
+  var whiteSpace = /\s+|-+/g;
+  var alphaChars = /[a-zA-Z]+/g;
+  str = str.replace(whiteSpace, "");
+  str = str.replace(alphaChars, "");
+
+
+  if (str.length === 11 || str.length === 13)
+  {
+    if(str[0] !== 1) return false;
+
+    var parenthesis = 0;
+    
+    for(var i = 0; i < str.length; i++)
+    {
+      if(str[i] == "(" || str[i] == ")"){
+        parenthesis ++;
+      }
+    }
+    if(parenthesis !== 2 || parenthesis !== 0){
+      return false;
+    }
   }
-  
-  telephoneCheck("555-555-5555");
-  
+
+  return true;
+}
+
+telephoneCheck("1 (555)-555-5555");
